ff45d0f8653441d4b8fa6a64fe85ae6df61f65ca
Tanner Rogalsky <tanner@tannerrogalsky.com>
2015-04-27 11:02:29 -0400

    test stub

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,26 @@
 var gulp = require('gulp');
 var browserify = require('gulp-browserify');
+var mocha = require('gulp-mocha');
+var generateSuite = require('gulp-mocha-browserify-suite');
 
 gulp.task('default', function() {
   gulp.src('./drl.js')
-    .pipe(browserify({
-      transform: ['babelify']
-    }))
+    .pipe(browserify({transform: ['babelify']}))
     .pipe(gulp.dest('./build'))
 
   gulp.src('./examples/test.js')
-    .pipe(browserify({
-      transform: ['babelify']
-    }))
+    .pipe(browserify({transform: ['babelify']}))
     .pipe(gulp.dest('./build'))
 });
 
 gulp.task('watch', ['default'], function(){
   gulp.watch(['./drl.js', './src/**/*.js', './examples/**/*.js'], ['default']);
+});
+
+gulp.task('test', function() {
+  return gulp.src('./spec/**/*', {read: false})
+    .pipe(generateSuite())
+    .pipe(browserify({transform: ['babelify']}))
+    .pipe(gulp.dest('./tmp'))
+    .pipe(mocha());
 });
