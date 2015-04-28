3947a19d49bae037c85b48114dd90a65e65e0b26
Tanner Rogalsky <tanner@tannerrogalsky.com>
2015-04-28 18:21:38 -0400

    that render function is going to get heavy so move it into its own file

--- src/drl.js
+++ src/drl.js
@@ -1,8 +1,6 @@
-import buildTree from './buildTree.js';
-import renderElement from './renderElement.js';
 import TexturePool from './texturePool.js';
 import createElement from './createElement.js';
-import Component from './component.js';
+import render from './render.js';
 
 var DRL = {
   createClass: function(spec) {
@@ -18,16 +16,7 @@ var DRL = {
     TexturePool.load(imagePaths, onCompleteCallback);
   },
 
-  render: function(element, canvas) {
-    var context = canvas.getContext('2d');
-    context.clearRect(0, 0, 600, 600);
-
-    var tree = {element: element, children: []};
-    buildTree(tree, '0', 0);
-
-    renderElement(tree, context);
-    return new Component(tree, context);
-  }
+  render: render
 };
 
 export default DRL;
