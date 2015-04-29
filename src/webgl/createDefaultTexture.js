311cb0b20ae6c1ce4de4c711cbf74df1a20b9663
Tanner Rogalsky <tanner@tannerrogalsky.com>
2015-04-29 15:56:55 -0400

    fix typo

--- src/webgl/createDefaultTexture.js
+++ src/webgl/createDefaultTexture.js
@@ -1,6 +1,6 @@
 var createDefaultTexture = function(context) {
   var data = new Uint8Array([255, 255, 255, 255]);
-  var texture = this.gl.createTexture();
+  var texture = context.createTexture();
   context.bindTexture(context.TEXTURE_2D, texture);
   context.texImage2D(context.TEXTURE_2D, 0, context.RGBA, 1, 1, 0, context.RGBA, context.UNSIGNED_BYTE, data);
   context.texParameteri(context.TEXTURE_2D, context.TEXTURE_MAG_FILTER, context.NEAREST);
